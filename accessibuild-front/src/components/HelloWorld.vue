550a487bb030cb19160a22ac558cd3ef6d15f325
John Xu <johnbxu@gmail.com>
2018-11-14 20:00:21 +0000

    vue set up with axios

---
+++ accessibuild-front/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+var axios = require('axios')
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted: function () {
+    axios.get('http://localhost:3000/users.json')
+      .then(response => {
+        this.msg = JSON.stringify(response.data)
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
