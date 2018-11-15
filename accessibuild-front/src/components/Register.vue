bf0de37948db9ecf08701df1b3c728f15d3c4999
Louis Riehl <louisriehl@gmail.com>
2018-11-15 21:26:56 +0000

    created register component

--- accessibuild-front/src/components/Register.vue
+++ accessibuild-front/src/components/Register.vue
@@ -1,5 +1,31 @@
 <template>
+
+  <div class="w-75 p-5">
   <h1>Welcome to the Login Page!</h1>
+
+  <form>
+    <div class="form-group">
+      <label>First Name</label>
+      <input type="text" name="first_name" class="form-control">
+    </div>
+    <div class="form-group">
+      <label>Last Name</label>
+      <input type="text" name="last_name" class="form-control">
+    </div>
+    <div class="form-group">
+      <label>Email</label>
+      <input type="text" name="email" class="form-control">
+    </div>
+    <div class="form-group">
+      <label>Password</label>
+      <input type="password" name="password" class="form-control">
+    </div>
+    <div class="form-group">
+      <label>Password Confirmation</label>
+      <input type="password" name="password_confirmation" class="form-control">
+    </div>
+  </form>
+</div>
 </template>
 
 <script>
