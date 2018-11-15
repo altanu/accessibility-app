17ee9b56f48fac6708835b231e18ec8dc9dda8e8
John Xu <johnbxu@gmail.com>
2018-11-15 18:06:24 +0000

    git index error fixed

---
+++ accessibuild-front/src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Locations from './components/Locations.vue'
+import Users from './components/Users.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/locations',
+      name: 'locations',
+      component: Locations
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: Users
+    }
+  ]
+})
