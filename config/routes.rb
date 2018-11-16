9dd2d77b7f6476633bb1682272a556c0b4ada366
Louis Riehl <louisriehl@gmail.com>
2018-11-16 14:50:14 +0000

    added new namespace, relocated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :trips
-  resources :reviews
-  resources :locations
-  resources :contacts
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :trips
+      resources :reviews
+      resources :locations
+      resources :contacts
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
