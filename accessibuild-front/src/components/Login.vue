c7a80b617737ca3a4a2e7b1fb6899d407a2f8f4c
Louis Riehl <louisriehl@gmail.com>
2018-11-16 16:40:07 +0000

    built login form

--- accessibuild-front/src/components/Login.vue
+++ accessibuild-front/src/components/Login.vue
@@ -1,4 +1,19 @@
+<template>
 
+  <div class="w-75 p-5">
+  <h1>Login</h1>
+
+  <form v-on:submit.prevent="onSubmit">
+    <div class="form-group">
+      <input type="text" name="email" placeholder="Email" class="form-control" v-model="email">
+    </div>
+    <div class="form-group">
+      <input type="password" name="password" placeholder="Password" class="form-control" v-model="password">
+    </div>
+    <button type="submit" class="btn btn-success btn-block">Login</button>
+  </form>
+</div>
+</template>
 
 <script>
 
