de581b398b68ab6fa03ea160eef06e64fefeb23b
Louis Riehl <louisriehl@gmail.com>
2018-11-16 15:19:45 +0000

    create v2 iteration for testing

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,19 @@ Rails.application.routes.draw do
       end
 
     end
+    namespace :v2 do
+      resources :users do
+        resources :contacts
+        resources :reviews
+        resources :trips
+      end
+
+      resources :locations do
+        resources :trips
+        resources :reviews
+      end
+
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
