c8690b58c719ca78360d6f1137458708eb6857db
Louis Riehl <louisriehl@gmail.com>
2018-11-16 14:55:03 +0000

    namespaces contacts and locations

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,51 +1,55 @@
-class ContactsController < ApplicationController
-  before_action :set_contact, only: [:show, :update, :destroy]
+module Api
+  module V1
+    class ContactsController < ApplicationController
+      before_action :set_contact, only: [:show, :update, :destroy]
 
-  # GET /contacts
-  def index
-    @contacts = Contact.all
+      # GET /contacts
+      def index
+        @contacts = Contact.all
 
-    render json: @contacts
-  end
+        render json: @contacts
+      end
 
-  # GET /contacts/1
-  def show
-    render json: @contact
-  end
+      # GET /contacts/1
+      def show
+        render json: @contact
+      end
 
-  # POST /contacts
-  def create
-    @contact = Contact.new(contact_params)
+      # POST /contacts
+      def create
+        @contact = Contact.new(contact_params)
 
-    if @contact.save
-      render json: @contact, status: :created, location: @contact
-    else
-      render json: @contact.errors, status: :unprocessable_entity
+        if @contact.save
+          render json: @contact, status: :created, location: @contact
+        else
+          render json: @contact.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /contacts/1
+      def update
+        if @contact.update(contact_params)
+          render json: @contact
+        else
+          render json: @contact.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /contacts/1
+      def destroy
+        @contact.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_contact
+          @contact = Contact.find(params[:id])
+        end
+
+        # Only allow a trusted parameter "white list" through.
+        def contact_params
+          params.require(:contact).permit(:user_id, :first_name, :last_name, :email, :phone_number, :emergency)
+        end
     end
   end
-
-  # PATCH/PUT /contacts/1
-  def update
-    if @contact.update(contact_params)
-      render json: @contact
-    else
-      render json: @contact.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /contacts/1
-  def destroy
-    @contact.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def contact_params
-      params.require(:contact).permit(:user_id, :first_name, :last_name, :email, :phone_number, :emergency)
-    end
 end
