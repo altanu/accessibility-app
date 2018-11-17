a6cd871c61c4d90a2d93bb054da89dea626a6313
Louis Riehl <louisriehl@gmail.com>
2018-11-17 18:28:39 +0000

    added new blank profile component

---
+++ accessibuild-front/src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Profile</h3>
+  <p> {{ msg }} </p>
+</template>
+
+<script>
+
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      msg: 'Component loaded!'
+    }
+  }
+}
+
+</script>
