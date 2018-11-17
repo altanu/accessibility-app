bee47a97b21fb4cb592e257b638e1af9b21aa20b
Louis Riehl <louisriehl@gmail.com>
2018-11-17 19:38:57 +0000

    can make axios request based on passed userId

--- accessibuild-front/src/components/Profile.vue
+++ accessibuild-front/src/components/Profile.vue
@@ -6,16 +6,17 @@
 
     <dl>
       <dt>First Name</dt>
-      <dd>{{ user.first_name }}</dd>
+      <dd>{{ user.first_name || 'No first name'}}</dd>
       <dt>Last Name</dt>
-      <dd>{{ user.last_name }}</dd>
+      <dd>{{ user.last_name || 'No last name'}}</dd>
       <dt>Email</dt>
-      <dd>{{ user.email }}</dd>
+      <dd>{{ user.email || 'No email'}}</dd>
     </dl>
   </div>
 </template>
 
 <script>
+var axios = require('axios')
 
 export default {
   name: 'Profile',
@@ -25,12 +26,14 @@ export default {
   data () {
     return {
       msg: 'Component loaded!',
-      user: {
-        first_name: "Test",
-        last_name: "Testerson",
-        email: "testing@email.com"
-      }
+      user: {}
     }
+  },
+  mounted: function () {
+    // console.log('Current loaded userid is' + this.userId)
+    axios.get('http://localhost:3000/api/v2/users/' + this.userId)
+      .then(response => (this.user = response.data))
+      .catch(error => console.log(error))
   }
 }
 
