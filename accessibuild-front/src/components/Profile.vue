2f666aa97ec2e49d85409b948c6387b67d8eaa9d
Louis Riehl <louisriehl@gmail.com>
2018-11-17 19:24:44 +0000

    linting fix

---
+++ accessibuild-front/src/components/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="profile">
+    <h3>Profile</h3>
+    <p> {{ msg }} </p>
+    <p> My parent says: {{ messageFromParent }}</p>
+
+    <dl>
+      <dt>Thing</dt>
+      <dd>Description of Thing</dd>
+      <dt>Thing</dt>
+      <dd>Description of Another Thing</dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Profile',
+  props: ['messageFromParent'],
+  data () {
+    return {
+      msg: 'Component loaded!'
+    }
+  }
+}
+
+</script>
