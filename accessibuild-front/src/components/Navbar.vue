ae795697cb1747613cd7a60e74d857cf67f627c8
altan <altan@unsal.ca>
2018-11-18 18:29:45 +0000

    removed unnecessary contacts button

--- accessibuild-front/src/components/Navbar.vue
+++ accessibuild-front/src/components/Navbar.vue
@@ -1,20 +1,19 @@
 <template>
   <nav class='navbar fixed-top navbar-light bg-light'>
     <div style="display:flex; width: 100%; flex-direction: row; justify-content: space-between">
-      <div style="flex-grow: 2"> 
-        <button @click='onClick("RightHome")' class='btn btn-outline-primary'>Home</button>
+      <div style="flex-grow: 1"> 
+        <button style="width: 7.5rem" @click='onClick("RightHome")' class='btn btn-outline-primary'>Home</button>
+        <button style="width: 7.5rem" @click='onClick("Profile")' class='btn btn-outline-primary' ref='profile' type='submit'>Profile</button>
       </div>
-      <div style="flex-grow: 6">
+      <div style="flex-grow: 2">
         <input id="pac-input" class="form-control" type="text" placeholder="🔍 Search keywords or addresses">
       </div>
-      <div style="flex-grow: 2; display:flex; justify-content: flex-end">
-        <button @click='onClick("Register")' class='btn btn-outline-primary' ref='register' type='submit'>Register</button>
+      <div style="flex-grow: 1; display:flex; justify-content: flex-end">
+        <button style="width: 5rem" @click='onClick("Register")' class='btn btn-outline-primary' ref='register' type='submit'>Register</button>
 
-        <button @click='onClick("Login")' class='btn btn-outline-primary' ref='login' type='submit'>Login</button>
+        <button style="width: 5rem" @click='onClick("Login")' class='btn btn-outline-primary' ref='login' type='submit'>Login</button>
 
-        <button @click='onClick("Profile")' class='btn btn-outline-primary' ref='profile' type='submit'>Profile</button>
-
-        <button @click='signOut' class='btn btn-outline-primary' type='submit'>Log Out</button>
+        <button style="width: 5rem" @click='signOut' class='btn btn-outline-primary' type='submit'>Log Out</button>
       </div>
     </div>
   </nav>
