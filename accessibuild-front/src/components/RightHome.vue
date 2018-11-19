7fe541bcbcc1864e2f4c539a3284f1d6d426aef0
altan <altan@unsal.ca>
2018-11-19 16:51:08 +0000

    using promises to get address

--- accessibuild-front/src/components/RightHome.vue
+++ accessibuild-front/src/components/RightHome.vue
@@ -2,6 +2,8 @@
   <div class="w-75 p-5">
     <div v-if="placesList.length == 0">
       <h4>Welcome</h4>
+      <p>We located you at {{currentPlace}}</p>
+      <p v-if="addressString">{{addressString}}</p>
       <p>What would you like to do today?</p>
     </div>
     <div>
@@ -24,12 +26,43 @@
 </template>
 
 <script>
+var geocoder = new google.maps.Geocoder
+var self = this
 export default {
   props: {
     onClick: Function,
     currentPlace: Object,
     placesList: Array
   },
-  name: 'RightHome'
+  data () {
+    return {
+      addressString: "TEST",
+      coordinates: this.currentPlace
+    }
+  },
+  mounted() {
+    this.updateAddress()
+  },
+  name: 'RightHome',
+  methods: {
+    updateAddress: () => {
+      var getAddress = new Promise(function(resolve, reject) {
+        console.log("called promise")
+        console.log("can I read currentPlace?", self.coordinates)
+        var address = ""
+        geocoder.geocode({'location': this.currentPlace}, function(results, status) {
+          console.log("status received", status)
+          console.log("called geocoder, got this result:", results)
+          address = results[0].formatted_address
+        })
+        resolve(address)
+      })
+      getAddress.then(data => {
+        console.log(data)
+        addressString = data
+      });
+    }
+  }
 }
+
 </script>
