1dc48cbe4ffb845979796a0f2a7dc068848844d1
Louis Riehl <louisriehl@gmail.com>
2018-11-20 11:33:13 -0500

    conditional rendering of header is based on $mq instead of css

--- accessibuild-front/src/components/Navbar.vue
+++ accessibuild-front/src/components/Navbar.vue
@@ -7,7 +7,7 @@
       <div style="display:flex; width: 100%; flex-direction: row; justify-content: space-between">
         <div style="flex-grow: 1;">
         <h3 style="width:100%; text-align: center">
-          <button id="accessibuild" style="width: 7.5rem" @click='onClick("RightHome")'>Accessibuild</button>
+          <button v-show="$mq === 'lg'" id="accessibuild" style="width: 7.5rem" @click='onClick("RightHome")'>Accessibuild</button>
         </h3>
         </div>
         <div style="flex-grow: 1" id="search-container">
