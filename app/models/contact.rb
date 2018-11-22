b6b649ce1c09067f437c8cf9e1037972df6a3c8a
John Xu <johnbxu@gmail.com>
2018-11-22 00:19:11 +0000

    trips model changed

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :trips
 
   validates :first_name, presence: true
   validates :email, presence: true
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :location
+
+  has_and_belongs_to_many :contacts
 end
