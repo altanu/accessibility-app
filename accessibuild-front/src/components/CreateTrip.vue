9ede7a5d9e3566d2b34362a61d57605114f9a6e9
John Xu <johnbxu@gmail.com>
2018-11-22 01:12:32 +0000

    made createtrip component

---
+++ accessibuild-front/src/components/CreateTrip.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='w-100' style='height: 100%; overflow: scroll;'>
+    <h1>Create your trip here!</h1>
+
+
+
+  </div>
+
+</template>
+<script type="text/javascript">
+var axios = require('axios')
+
+export default {
+  name: 'CreateTrip'
+
+
+
+}
+
+</script>
