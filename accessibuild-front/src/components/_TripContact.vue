9221bb5fa1cd3752009569beb9c718c105aa174d
Louis Riehl <louisriehl@gmail.com>
2018-11-25 18:43:10 +0000

    added placeholder code for tripcontact

--- accessibuild-front/src/components/_TripContact.vue
+++ accessibuild-front/src/components/_TripContact.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <div class="contact-info card col" style="padding: 0;">
+      <div class="card-header">
+        <p>{{contact.first_name}}</p>
+      </div>
+      <div class="card-body">
+        <p>{{contact.email}}</p>
+        <p v-if="contact.emergency">{{contact.first_name}} is one of your emergency contacts!</p>
+        <p v-else >{{contact.first_name}} isn't an emergency contact.</p>
+      </div>
+    </div>
+    <div class="contact-buttons card col-sm-4" style="display: flex; flex-direction: column; padding: 0;">
+      <button @click="addContactToTrip(contact)" style="flex-grow: 1;" class="btn btn-outline-primary round-button">Add</button>
+      <button @click="removeContactFromTrip(contact)" style="flex-grow: 1;" class="btn btn-outline-primary round-button">Remove</button>
+    </div>
   </div>
 </template>
 
