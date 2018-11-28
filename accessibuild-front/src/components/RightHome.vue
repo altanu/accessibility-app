5b344a41cf435f8b780d1d615ca89a63347eb241
altan <altan@unsal.ca>
2018-11-28 22:26:15 +0000

    search button stuff

--- accessibuild-front/src/components/RightHome.vue
+++ accessibuild-front/src/components/RightHome.vue
@@ -2,13 +2,13 @@
   <div id='right-home'>
     <button @click.self="$emit('drop-right')" v-show="isFocused && $mq === 'sm'" class="btn btn-outline-primary btn-block">Hide</button>
     <div @click="$emit('lift-right')" v-if="placesList.length == 0">
+      <h2>Accessibl helps plan your trip</h2>
+
       <div class="alert alert-dark">
         <p>We located you near:</p>
         <b>{{userDetectedPlace.formatted_address}}</b>
       </div>
       
-      <h1>Accessibl helps plan your trip</h1>
-
       <p class="lead">
         Accessibl was built to help people with reduced mobility plan their outings. We keep track of which locations have a
         <strong>ramp</strong>, <strong>accessible bathroom</strong> or <strong>reserved parking</strong>, so you know what to expect
@@ -17,15 +17,8 @@
       <p class="lead">
         The reviews for locations are user generated, and you can see comments people have left on the accessibility status of each.
         You will see the most recent data we have on the physical facilities, so if things change since you've last been somewhere, you
-        know ahead of time. If you want to provide updates for a location, or just add a new one, all you have to do is make an account, 
-        and then you will be able to review any location.
+        know ahead of time.
       </p>
-      <p class="lead">
-        Having an account will also allow you to add contacts we can notify on your behalf when you plan a trip using Accessibl. If you
-        like, we can send updates to them before, during and after your trip so your companions will now when and where to meet you.
-      </p>
-
-      <button class='btn btn-outline-primary round-button' @click="focusOnSearch">Search</button>
     </div>
     <div @click="$emit('lift-right')" v-if="placesList.length > 0">
       <p>We found the following results for you:</p>
@@ -54,16 +47,6 @@ export default {
   name: 'RightHome',
   components: {
     Location
-  },
-  methods: {
-    focusOnSearch: function () {
-      var searchBar = document.getElementById('pac-input')
-      searchBar.style.border = '3px solid black'
-      searchBar.focus()
-      setTimeout(function () {
-        searchBar.style.border = '1px solid grey'
-      }, 3000)
-    }
   },
   mounted() {
     this.$parent.$on('app-refresh', this.refreshMap)
